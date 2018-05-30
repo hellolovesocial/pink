e14c6a55cedac8f2e3a694c0556cad54c0f656fa
hellolovesocial <taylor.minako375@gmail.com>
2018-05-31 05:40:56 +0900

    Added some pages

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter)
+app.use('/about', aboutRouter)
+app.use('/home', homeRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
